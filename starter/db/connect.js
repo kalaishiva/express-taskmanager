669c7a731af095abd16b0c4c6e2ef2346368e85b
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-11-02 18:43:47 +0100

    mongoDb is connected fully

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
 
-const connectionString = "mongodb+srv://Kalai:Kalai@mongoose@cluster0.4bzqp2i.mongodb.net/Task-Manager?retryWrites=true&w=majority";
+const connectionString = "mongodb+srv://Kalai:kalai12345@cluster0.4bzqp2i.mongodb.net/Task-Manager?retryWrites=true&w=majority";
 
 mongoose
-    .connect(connectionString)
+    .connect(connectionString, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
     .then(() => console.log("Connected to the DB....."))
     .catch((err) => console.log(err))
