639bfcb0b35d08d6b066481d727aca3fe950e91e
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-11-02 18:33:27 +0100

    Mongoose setup in the taskmanger

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -1,1 +1,9 @@
-const connectionString = "mongodb+srv://Kalai:<password>@cluster0.4bzqp2i.mongodb.net/?retryWrites=true&w=majority";
+const mongoose = require('mongoose');
+
+
+const connectionString = "mongodb+srv://Kalai:Kalai@mongoose@cluster0.4bzqp2i.mongodb.net/Task-Manager?retryWrites=true&w=majority";
+
+mongoose
+    .connect(connectionString)
+    .then(() => console.log("Connected to the DB....."))
+    .catch((err) => console.log(err))
